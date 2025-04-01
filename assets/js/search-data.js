d08e1bfe32b91d80717f2f84b21e640a5203a54b
masakitakeuchi-uosaka <takeuchi.masaki.fpq@ecs.osaka-u.ac.jp>
2025-04-01 14:28:15 +0000

    Deploying to gh-pages from @ masakitakeuchi-uosaka/masakitakeuchi-uosaka.github.io@76ea70d784d9583fef3b85539b7e117c6d083c79 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -33,35 +33,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%61%6B%65%75%63%68%69.%6D%61%73%61%6B%69.%66%70%71_%61%74_%65%63%73.%6F%73%61%6B%61-%75.%61%63.%6A%70", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/masakitakeuchi-uosaka", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=2rn6ex0AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
