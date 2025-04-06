54531c7c4e206119a322890da454364bfaaa99ae
masakitakeuchi-uosaka <takeuchi.masaki.fpq@ecs.osaka-u.ac.jp>
2025-04-06 09:42:09 +0000

    Deploying to gh-pages from @ masakitakeuchi-uosaka/masakitakeuchi-uosaka.github.io@840ba7bfdcce31e11396e7d8c62b144a42fb83d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -406,6 +406,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
             },},{
         id: 'social-email',
         title: 'email',
