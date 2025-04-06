2155dc8be63fdf7221eb57b361dd098cd50ada3b
masakitakeuchi-uosaka <takeuchi.masaki.fpq@ecs.osaka-u.ac.jp>
2025-04-06 09:52:05 +0000

    Deploying to gh-pages from @ masakitakeuchi-uosaka/masakitakeuchi-uosaka.github.io@9d65535ac0b6cf8688c87bee8335aaf59859308c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -406,6 +406,11 @@ ninja.data = [{
           description: "IEEE TVCG 2024, IEEE VR 2024",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
             },},{
         id: 'social-email',
         title: 'email',
