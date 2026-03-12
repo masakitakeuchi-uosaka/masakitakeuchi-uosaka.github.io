212058e3549bba90efc01ed2912031caa1729953
masaki-takeuchi-info <takeuchi.masaki.fpq@ecs.osaka-u.ac.jp>
2026-03-12 07:47:07 +0000

    Deploying to gh-pages from @ masaki-takeuchi-info/masaki-takeuchi-info.github.io@3d9878d0385948ee4f11f8fb2b94f614b571099d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,10 +398,12 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-released-my-website",
-          title: 'Released my website.',
+            },},{id: "news-announcement-1",
+          title: 'Announcement_1',
           description: "",
-          section: "News",},{id: "news-gave-a-poster-presentation-at-inaugural-texture-forum-第1回質感フォーラム",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_1/";
+            },},{id: "news-gave-a-poster-presentation-at-inaugural-texture-forum-第1回質感フォーラム",
           title: 'Gave a poster presentation at Inaugural Texture Forum (第1回質感フォーラム).',
           description: "",
           section: "News",},{id: "news-gave-an-oral-presentation-at-vision-society-of-japan-2025-summer-meeting-日本視覚学会2025年夏季大会",
